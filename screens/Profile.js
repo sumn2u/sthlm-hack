156b97c9072cd35a7394d7465302b97d81b16fee
Duy Nguyen <nguyenduy010798@gmail.com>
2020-11-07 16:21:10 +0200

    :sparkles: Slight modification of profile screen

--- screens/Profile.js
+++ screens/Profile.js
@@ -13,6 +13,10 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { Icon } from '../components'
 import { Images, materialTheme } from '../constants'
 import { HeaderHeight } from '../constants/utils'
+import { AntDesign } from '@expo/vector-icons'
+import { FontAwesome } from '@expo/vector-icons'
+
+import profile from '../assets/images/profile.jpg'
 
 const { width, height } = Dimensions.get('screen')
 const thumbMeasure = (width - 48 - 32) / 3
@@ -23,30 +27,16 @@ export default class Profile extends React.Component {
       <Block flex style={styles.profile}>
         <Block flex>
           <ImageBackground
-            source={{ uri: Images.Profile }}
+            source={profile}
             style={styles.profileContainer}
             imageStyle={styles.profileImage}
           >
             <Block flex style={styles.profileDetails}>
               <Block style={styles.profileTexts}>
                 <Text color="white" size={28} style={{ paddingBottom: 8 }}>
-                  Rachel Brown
+                  Alexander
                 </Text>
                 <Block row space="between">
-                  <Block row>
-                    <Block middle style={styles.pro}>
-                      <Text size={16} color="white">
-                        Pro
-                      </Text>
-                    </Block>
-                    <Text color="white" size={16} muted style={styles.seller}>
-                      Seller
-                    </Text>
-                    <Text size={16} color={materialTheme.COLORS.WARNING}>
-                      4.8{' '}
-                      <Icon name="shape-star" family="GalioExtra" size={14} />
-                    </Text>
-                  </Block>
                   <Block>
                     <Text color={theme.COLORS.MUTED} size={16}>
                       <Icon
@@ -55,7 +45,7 @@ export default class Profile extends React.Component {
                         color={theme.COLORS.MUTED}
                         size={16}
                       />
-                      {` `} Los Angeles, CA
+                      {` `} Helsinki, Finland
                     </Text>
                   </Block>
                 </Block>
@@ -75,15 +65,7 @@ export default class Profile extends React.Component {
                   36
                 </Text>
                 <Text muted size={12}>
-                  Orders
-                </Text>
-              </Block>
-              <Block middle>
-                <Text bold size={12} style={{ marginBottom: 8 }}>
-                  5
-                </Text>
-                <Text muted size={12}>
-                  Bids & Offers
+                  Eco Point
                 </Text>
               </Block>
               <Block middle>
@@ -91,34 +73,28 @@ export default class Profile extends React.Component {
                   2
                 </Text>
                 <Text muted size={12}>
-                  Messages
+                  Badges
+                </Text>
+              </Block>
+              <Block middle>
+                <Text bold size={12} style={{ marginBottom: 8 }}>
+                  2
+                </Text>
+                <Text muted size={12}>
+                  Notifications
                 </Text>
               </Block>
             </Block>
-            <Block
-              row
-              space="between"
-              style={{ paddingVertical: 16, alignItems: 'baseline' }}
-            >
-              <Text size={16}>Recently viewed</Text>
-              <Text
-                size={12}
-                color={theme.COLORS.PRIMARY}
-                onPress={() => this.props.navigation.navigate('Home')}
-              >
-                View All
-              </Text>
-            </Block>
             <Block style={{ paddingBottom: -HeaderHeight * 2 }}>
-              <Block row space="between" style={{ flexWrap: 'wrap' }}>
-                {Images.Viewed.map((img, imgIndex) => (
-                  <Image
-                    source={{ uri: img }}
-                    key={`viewed-${img}`}
-                    resizeMode="cover"
-                    style={styles.thumb}
-                  />
-                ))}
+              <Block column space="between" center style={{ flexWrap: 'wrap' }}>
+                <Block row middle>
+                  <AntDesign name="staro" size={24} color="black" />
+                  <Text>CO2 destroyer</Text>
+                </Block>
+                <Block row middle>
+                  <FontAwesome name="superpowers" size={24} color="black" />
+                  <Text>Green superman</Text>
+                </Block>
               </Block>
             </Block>
           </ScrollView>
