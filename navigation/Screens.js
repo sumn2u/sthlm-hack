a498ce9ccfc5a3a97cd50323fc3fe65ef51e3295
Duy Nguyen <nguyenduy010798@gmail.com>
2020-11-08 00:26:14 +0200

    fix conflicts

--- navigation/Screens.js
+++ navigation/Screens.js
@@ -14,9 +14,6 @@ import CustomDrawerContent from './Menu'
 import { Icon, Header } from '../components'
 import { Images, materialTheme } from '../constants/'
 
-import CustomDrawerContent from './Menu'
-import { Icon, Header } from '../components'
-import { Images, materialTheme } from '../constants/'
 import Leaderboard from '../screens/Leaderboard'
 
 const Stack = createStackNavigator()
@@ -29,6 +26,8 @@ const profile = {
   plan: 'Pro',
   rating: 4.8,
 }
+
+const { width } = Dimensions.get('screen')
 
 function ProfileStack(props) {
   return (
